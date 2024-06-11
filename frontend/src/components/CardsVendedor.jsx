7611126b56352fdee27411241808135471e072b0
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2024-06-11 16:39:17 -0500

    pantalla tienda vendedor

--- frontend/src/components/CardsVendedor.jsx
+++ frontend/src/components/CardsVendedor.jsx
@@ -6,7 +6,7 @@ function CardsVendedor() {
         {/*Aqui empiezan las cards*/}
        <div>
        <div className="flex justify-center  ">
-       <div className="flex flex-col mx-[1%]">
+            <div className="flex flex-col mx-[1%]">
                 <div className="bg-[#D9D9D9] border-b border-b-[#6287AF]  w-[11vw] h-[28vh]">
                     <h2 className="text-center">IMAGEN</h2>
                 </div>
@@ -15,11 +15,11 @@ function CardsVendedor() {
                         <h3 className="text-[#868686] p-[2%] mx-[3%] text-xs ">Categoria del producto por nombre de la tienda</h3>
                     <div className="flex justify-between">
                         <h3 className="text-[#2374AB] text-xs p-[2%] px-[5%]">MXN$ 200.00</h3>
-                    <div className="bg-[#F6452C] w-[2vw] h-[4vh]  rounded-md">
-                        <h2 className="text-center">🗑</h2>
+                    <div className="bg-[#304D6D] w-[2vw] h-[4vh]  rounded-md">
+                        <h2 className="text-center">✒️</h2>
                     </div>
                     <div className="bg-[#F6452C] w-[2vw] h-[4vh] mx-[6%] rounded-md">
-                        <h2 className="text-center">🗑</h2>
+                        <img className="w-[5vw] h-[4vh]" src="https://cdn-icons-png.flaticon.com/128/3541/3541990.png"/>
                     </div>
                     </div>
                 </div>
@@ -33,8 +33,11 @@ function CardsVendedor() {
                         <h3 className="text-[#868686] p-[2%] mx-[3%] text-xs ">Categoria del producto por nombre de la tienda</h3>
                     <div className="flex justify-between">
                         <h3 className="text-[#2374AB] text-xs p-[2%] px-[5%]">MXN$ 200.00</h3>
-                    <div className="bg-[#70C5BB] w-[2vw] h-[4vh] mx-[6%] rounded-md">
-                        <h2 className="text-center">💬</h2>
+                    <div className="bg-[#304D6D] w-[2vw] h-[4vh]  rounded-md">
+                        <h2 className="text-center">✒️</h2>
+                    </div>
+                    <div className="bg-[#F6452C] w-[2vw] h-[4vh] mx-[6%] rounded-md">
+                        <img className="w-[5vw] h-[4vh]" src="https://cdn-icons-png.flaticon.com/128/3541/3541990.png"/>
                     </div>
                     </div>
                 </div>
@@ -48,8 +51,11 @@ function CardsVendedor() {
                         <h3 className="text-[#868686] p-[2%] mx-[3%] text-xs ">Categoria del producto por nombre de la tienda</h3>
                     <div className="flex justify-between">
                         <h3 className="text-[#2374AB] text-xs p-[2%] px-[5%]">MXN$ 200.00</h3>
-                    <div className="bg-[#70C5BB] w-[2vw] h-[4vh] mx-[6%] rounded-md">
-                        <h2 className="text-center">💬</h2>
+                    <div className="bg-[#304D6D] w-[2vw] h-[4vh]  rounded-md">
+                        <h2 className="text-center">✒️</h2>
+                    </div>
+                    <div className="bg-[#F6452C] w-[2vw] h-[4vh] mx-[6%] rounded-md">
+                        <img className="w-[5vw] h-[4vh]" src="https://cdn-icons-png.flaticon.com/128/3541/3541990.png"/>
                     </div>
                     </div>
                 </div>
@@ -63,8 +69,11 @@ function CardsVendedor() {
                         <h3 className="text-[#868686] p-[2%] mx-[3%] text-xs ">Categoria del producto por nombre de la tienda</h3>
                     <div className="flex justify-between">
                         <h3 className="text-[#2374AB] text-xs p-[2%] px-[5%]">MXN$ 200.00</h3>
-                    <div className="bg-[#70C5BB] w-[2vw] h-[4vh] mx-[6%] rounded-md">
-                        <h2 className="text-center">💬</h2>
+                    <div className="bg-[#304D6D] w-[2vw] h-[4vh]  rounded-md">
+                        <h2 className="text-center">✒️</h2>
+                    </div>
+                    <div className="bg-[#F6452C] w-[2vw] h-[4vh] mx-[6%] rounded-md">
+                        <img className="w-[5vw] h-[4vh]" src="https://cdn-icons-png.flaticon.com/128/3541/3541990.png"/>
                     </div>
                     </div>
                 </div>
@@ -79,8 +88,11 @@ function CardsVendedor() {
                         <h3 className="text-[#868686] p-[2%] mx-[3%] text-xs ">Categoria del producto por nombre de la tienda</h3>
                     <div className="flex justify-between">
                         <h3 className="text-[#2374AB] text-xs p-[2%] px-[5%]">MXN$ 200.00</h3>
-                    <div className="bg-[#70C5BB] w-[2vw] h-[4vh] mx-[6%] rounded-md">
-                        <h2 className="text-center">💬</h2>
+                    <div className="bg-[#304D6D] w-[2vw] h-[4vh]  rounded-md">
+                        <h2 className="text-center">✒️</h2>
+                    </div>
+                    <div className="bg-[#F6452C] w-[2vw] h-[4vh] mx-[6%] rounded-md">
+                        <img className="w-[5vw] h-[4vh]" src="https://cdn-icons-png.flaticon.com/128/3541/3541990.png"/>
                     </div>
                     </div>
                 </div>
@@ -94,8 +106,11 @@ function CardsVendedor() {
                         <h3 className="text-[#868686] p-[2%] mx-[3%] text-xs ">Categoria del producto por nombre de la tienda</h3>
                     <div className="flex justify-between">
                         <h3 className="text-[#2374AB] text-xs p-[2%] px-[5%]">MXN$ 200.00</h3>
-                    <div className="bg-[#70C5BB] w-[2vw] h-[4vh] mx-[6%] rounded-md">
-                        <h2 className="text-center">💬</h2>
+                    <div className="bg-[#304D6D] w-[2vw] h-[4vh]  rounded-md">
+                        <h2 className="text-center">✒️</h2>
+                    </div>
+                    <div className="bg-[#F6452C] w-[2vw] h-[4vh] mx-[6%] rounded-md">
+                        <img className="w-[5vw] h-[4vh]" src="https://cdn-icons-png.flaticon.com/128/3541/3541990.png"/>
                     </div>
                     </div>
                 </div>
@@ -109,8 +124,11 @@ function CardsVendedor() {
                         <h3 className="text-[#868686] p-[2%] mx-[3%] text-xs ">Categoria del producto por nombre de la tienda</h3>
                     <div className="flex justify-between">
                         <h3 className="text-[#2374AB] text-xs p-[2%] px-[5%]">MXN$ 200.00</h3>
-                    <div className="bg-[#70C5BB] w-[2vw] h-[4vh] mx-[6%] rounded-md">
-                        <h2 className="text-center">💬</h2>
+                    <div className="bg-[#304D6D] w-[2vw] h-[4vh]  rounded-md">
+                        <h2 className="text-center">✒️</h2>
+                    </div>
+                    <div className="bg-[#F6452C] w-[2vw] h-[4vh] mx-[6%] rounded-md">
+                        <img className="w-[5vw] h-[4vh]" src="https://cdn-icons-png.flaticon.com/128/3541/3541990.png"/>
                     </div>
                     </div>
                 </div>
