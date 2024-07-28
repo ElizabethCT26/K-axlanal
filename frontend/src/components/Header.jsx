8a9508e295ad7179edaa20cfb19419d745ecd43f
Octavio Cruz <dorersempai@gmil.com>
2024-07-28 16:27:52 -0500

    notifications

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -33,7 +33,6 @@ function Header() {
     localStorage.setItem('darkMode', !darkMode);
   }
 
-
   const fetchProfile = async () => {
     console.log(userId)
     const response = await axios.get(`https://localhost:8082/profiles/${userId}`) 
@@ -90,17 +89,35 @@ function Header() {
       }
       {
         useModal && (
-          <div className={` ${darkMode ? ('bg-darkMainBackground border-darkCardBg') : ('bg-cardBg border-[#CECECE]')} border-l border-b rounded-sm w-[14vw] flex-fit fixed right-0 top-0 flex flex-wrap z-50`}>
-            <div className='w-full h-full relative rounded-sm'>
+          <div className={` ${darkMode ? ('bg-darkMainBackground border-darkCardBg') : ('bg-cardBg border-[#CECECE]')} border-l border-b rounded-sm w-[14vw] flex-fit h-[50vh] fixed right-0 top-0 flex flex-wrap z-50`}>
+            <div className='w-full h-full relative rounded-sm '>
 
-              <div className=' flex justify-center bg-cardBackground'>
-                <div className='w-full h-[8vh] px-[.5vw] py-[1vh]'>
-                  <button type='button' className={` ${darkMode ? ('bg-darkCardBottom') : ('bg-cardBottom')}  rounded-full flex flex-wrap  w-[1.8vw] h-[4vh] items-center justify-center`} onClick={() => setUserModal(false)}>
-                  x
-                  </button>
+              <div className=' flex justify-center flex-wrap items-center'>
+                <div className='w-full flex h-[8vh] px-[.5vw] py-[1vh] relative'>
+
+
+                {
+                    displayNoti ? (
+                      <>
+                        <button type='button' className={` ${darkMode ? ('bg-darkCardBottom') : ('bg-cardBottom')}  rounded-full flex flex-wrap  w-[1.8vw] h-[4vh] absolute items-center justify-center`} onClick={() => setDisplayNoti(false)}>
+                        &lt;
+                        </button>
+                        
+                        <h2 className='w-full h-full items-center flex text-center justify-center text-sm'>Notificaciones</h2>
+                      </>
+                    ) : (
+                      <button type='button' className={` ${darkMode ? ('bg-darkCardBottom') : ('bg-cardBottom')}  rounded-full flex flex-wrap  w-[1.8vw] h-[4vh] absolute items-center justify-center`} onClick={() => setUserModal(false)}>
+                      x
+                      </button>
+                    )
+                  }
                 </div>
+
+
                 
-                <div className='w-[3.7vw] h-[8vh] rounded-full border-2 bg-[#3A4E64] border-[#341CA7] absolute top-[4.5vh]'>
+              {
+                !displayNoti &&  (
+                  <div className='w-[3.7vw] h-[8vh] rounded-full border-2 bg-[#3A4E64] border-[#341CA7] absolute top-[4.5vh]'>
                   {
                     data ? (
                       data.map((profile, index) => (
@@ -108,64 +125,71 @@ function Header() {
                       ))
                     ) : ('a')
                   }
-                </div>
+                </div> 
+                )
+              }
               </div>
-              <div className={`bg-blue-400 ${darkMode ? ('bg-darkCardBg border-darkPrices') : ('bg-cardBg border-prices')} pb-[3vh] pt-[6vh] w-full flex flex-wrap gap-[1vh] bg-cardBottom rounded-b-sm`}>
-                <div className='w-full flex items-center justify-center pb-[1vh] '>
+              <div className={` ${darkMode ? ('bg-darkCardBg border-darkPrices') : ('bg-cardBg border-prices')} pb-[3vh] pt-[6vh] ${ displayNoti && ('h-[84%]')  } w-full flex flex-wrap gap-[1vh] bg-cardBottom rounded-b-sm`}>
                   {
-                    data ? (
-                      data.map((profile, index) => (
-                        <div className='w-full flex flex-col items-center'>
-                          <h2 key={index}>Hola {profile.nombre} {profile.apellido}!</h2>
-                          <h2 key={index + 1} className='font-light text-xs'>{profile.correo}</h2>
+                    displayNoti ? (<></>) : (
+                      <>
+                      <div className='w-full flex items-center justify-center pb-[1vh] '>
+                      {
+                        data ? (
+                          data.map((profile, index) => (
+                            <div className='w-full flex flex-col items-center'>
+                              <h2 key={index}>Hola {profile.nombre} {profile.apellido}!</h2>
+                              <h2 key={index + 1} className='font-light text-xs'>{profile.correo}</h2>
+                            </div>
+                          ))
+                          ) : (
+                            <h2 >Hola!</h2>
+                          )
+                      }
+                    </div>
+    
+                    <button type='button' className='w-full flex flex-wrap justify-between text-left mx-[1.5vw] px-[1vw] py-[1%] text-sm font-light border-b-2 border-b-darkAccents pb-[1vh]' onClick={() => setDisplayNoti(true)}>
+                      <h2 className=''>Notificaciones</h2>
+                        <div className='w-[1.2vw]'>
+                          <img src={ darkMode ? (darkBell) : (lightBell)} className='w-full h-full object-cover rounded-full'/>
                         </div>
-                      ))
-                      ) : (
-                        <h2 >Hola!</h2>
-                      )
+                    </button>
+    
+                    <button type='button' className='w-full  mx-[1.5vw] px-[1vw] py-[1%] text-sm  font-light border-b-2 border-b-darkAccents  pb-[1vh]'>
+                      <Link to='/perfil' className='flex flex-wrap justify-between'>
+                        <h2 className=''>Ver perfil</h2>
+                        <div className='w-[1.2vw]'>
+                          <img src={ darkMode ? (darkProfileCharacter) : (lightProfileCharacter)} className='w-full h-full object-cover rounded-full'/>
+                        </div>
+                      </Link>
+                    </button>
+    
+                    <button type='button' className='w-full flex flex-wrap justify-between text-left mx-[1.5vw] px-[1vw] py-[1%] text-sm font-light border-b-2 border-b-darkAccents  pb-[1vh]' onClick={() => handleModeChange()}>
+                      <h2 className=''>{ darkMode ? ('Modo claro') : ('Modo oscuro') }</h2>
+                        <div className='w-[1.2vw]'>
+                          <img src={ darkMode ? (lightModeIcon) : (darkModeIcon)} className='w-full h-full object-cover rounded-full'/>
+                        </div>
+                    </button>
+    
+                    
+                    <button type='button' className='w-full  mx-[1.5vw] px-[1vw] py-[1%] text-sm  font-light border-b-2 border-b-darkAccents  pb-[1vh]'>
+                      <Link to='/crear-tienda' className='flex flex-wrap justify-between'>
+                        <h2 className=''>Crear tienda</h2>
+                        <div className='w-[1.2vw]'>
+                          <img src={ darkMode ? (storeDark) : (storeLight)} className='w-full h-full object-cover rounded-full'/>
+                        </div>
+                      </Link>
+                    </button>
+    
+                    <button type='button' className={`${darkMode && ('font-medium')} w-full flex flex-wrap justify-between text-left mx-[1.5vw] px-[1vw] py-[1%] text-sm border-b-2 border-red-500 text-red-500  pb-[1vh]`} onClick={() => logOut()}>
+                      <h2 className=''>Cerrar sesión</h2>
+                        <div className='w-[1.2vw]'>
+                          <img src={Close} className='w-full h-full object-cover rounded-full'/>
+                        </div>
+                    </button>
+                      </>
+                    )
                   }
-                </div>
-
-                <button type='button' className='w-full flex flex-wrap justify-between text-left mx-[1.5vw] px-[1vw] py-[1%] text-sm font-light border-b-2 border-b-darkAccents pb-[1vh]' onClick={() => handleModeChange()}>
-                  <h2 className=''>Notificaciones</h2>
-                    <div className='w-[1.2vw]'>
-                      <img src={ darkMode ? (darkBell) : (lightBell)} className='w-full h-full object-cover rounded-full'/>
-                    </div>
-                </button>
-
-                <button type='button' className='w-full  mx-[1.5vw] px-[1vw] py-[1%] text-sm  font-light border-b-2 border-b-darkAccents  pb-[1vh]'>
-                  <Link to='/perfil' className='flex flex-wrap justify-between'>
-                    <h2 className=''>Ver perfil</h2>
-                    <div className='w-[1.2vw]'>
-                      <img src={ darkMode ? (darkProfileCharacter) : (lightProfileCharacter)} className='w-full h-full object-cover rounded-full'/>
-                    </div>
-                  </Link>
-                </button>
-
-                <button type='button' className='w-full flex flex-wrap justify-between text-left mx-[1.5vw] px-[1vw] py-[1%] text-sm font-light border-b-2 border-b-darkAccents  pb-[1vh]' onClick={() => handleModeChange()}>
-                  <h2 className=''>{ darkMode ? ('Modo claro') : ('Modo oscuro') }</h2>
-                    <div className='w-[1.2vw]'>
-                      <img src={ darkMode ? (lightModeIcon) : (darkModeIcon)} className='w-full h-full object-cover rounded-full'/>
-                    </div>
-                </button>
-
-                
-                <button type='button' className='w-full  mx-[1.5vw] px-[1vw] py-[1%] text-sm  font-light border-b-2 border-b-darkAccents  pb-[1vh]'>
-                  <Link to='/crear-tienda' className='flex flex-wrap justify-between'>
-                    <h2 className=''>Crear tienda</h2>
-                    <div className='w-[1.2vw]'>
-                      <img src={ darkMode ? (storeDark) : (storeLight)} className='w-full h-full object-cover rounded-full'/>
-                    </div>
-                  </Link>
-                </button>
-
-                <button type='button' className={`${darkMode && ('font-medium')} w-full flex flex-wrap justify-between text-left mx-[1.5vw] px-[1vw] py-[1%] text-sm border-b-2 border-red-500 text-red-500  pb-[1vh]`} onClick={() => logOut()}>
-                  <h2 className=''>Cerrar sesión</h2>
-                    <div className='w-[1.2vw]'>
-                      <img src={Close} className='w-full h-full object-cover rounded-full'/>
-                    </div>
-                </button>
-
               </div>
             </div>
           </div>
